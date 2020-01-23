56edf697154e27b1f71b00b210acc5a94b51e2c8
Emarion22 <60225307+Emarion22@users.noreply.github.com>
2020-01-23 09:04:11 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+$("p").hide();
+});
+
+
+$(".happy").click(function() {
+$("p").fadeIn();
+});
+
+$("p").click(function(){
+    $("p").text("click on header");
+});
+
+$("h1").click(function(){
+    $("h1").css("color","blue");
+});
+
+    
+    
+
+
+
+
+
+
